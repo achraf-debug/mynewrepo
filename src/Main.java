ba6d4f15f96ec8c4e30525b4224eda82fd7ac94a
Achraf Dridi <achrafdridi602@gmail.com>
2023-03-25 17:40:42 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(args.length);
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        }
+    }
+}
